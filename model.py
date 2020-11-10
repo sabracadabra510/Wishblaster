8282e732b09aa8dda04813e5de60597587354212
Sabby510 <sabrinakporter@gmail.com>
2020-11-09 16:20:07 -0800

    class added

--- model.py
+++ model.py
@@ -19,3 +19,21 @@ class User(db.Model):
 
     def __repr__(self):
         return f'<User user_id={self.user_id} email={self.email}>'
+
+
+class Family(db.Model):
+   """Family member's table"""
+    __tablename__ = "family"
+
+    family_id = db.Column(db.Integer,
+                autoincrement = True,
+                primary_key = True)
+
+    user_id = db.Colum(db.Integer, db.ForeignKey('users.user_id'))
+    full_name = db.Column(db.String)
+    birth_date = db.Column(db.Datetime)
+    relationship_to_user = db.Column(db.Integer)
+    image_upload =db.Column(db.String)
+
+    def __repr__(self):
+        return f'<Family family_id ={self.family_id} full_name={self.full_name}>'
