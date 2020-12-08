0eb3a0615dbcf12ce6e262a64ca1f075cf342741
Sabrina Porter <sabrinakporter@gmail.com>
2020-12-08 22:06:11 +0000

    changes to formatting and forms

--- static/js/action.js
+++ static/js/action.js
@@ -8,7 +8,7 @@ $('#view-list').on('submit', (evt) =>{
         console.log(res);
         for (const item of res) {
             $('#magic').append(`<p>Item: ${item.item_name}</p>`);
-            $('#magic').append(`      Website Link: ${item.item_link}`);
+            $('#magic').append(`<p>Website Link: ${item.item_link}</p>`);
         }
       });
 })
