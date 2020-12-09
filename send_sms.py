01fcd67a2ac65da35cfb7087d0e9192aa5fc0156
Sabrina Porter <sabrinakporter@gmail.com>
2020-12-09 19:13:38 +0000

    updates

--- send_sms.py
+++ send_sms.py
@@ -4,8 +4,8 @@ from twilio.rest import Client
 
 # Your Account Sid and Auth Token from twilio.com/console
 # and set the environment variables. See http://twil.io/secure
-account_sid = 'AC8c4c6bbbe9876c4a960bd59b2bcf6f4f'
-auth_token = '7fdec5dc642fcc0ff33fd1e08b48565b'
+account_sid = 
+auth_token = 
 client = Client(account_sid, auth_token)
 
 message = client.messages \
