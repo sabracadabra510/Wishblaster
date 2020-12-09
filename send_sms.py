10d3775f624eb3f751d7bb2b5a8af449ec9aebba
Sabrina Porter <sabrinakporter@gmail.com>
2020-12-09 04:23:08 +0000

    final commits

--- send_sms.py
+++ send_sms.py
@@ -5,12 +5,12 @@ from twilio.rest import Client
 # Your Account Sid and Auth Token from twilio.com/console
 # and set the environment variables. See http://twil.io/secure
 account_sid = 'AC8c4c6bbbe9876c4a960bd59b2bcf6f4f'
-auth_token = '51d8175b66f818963269de447c87cd83'
+auth_token = '7fdec5dc642fcc0ff33fd1e08b48565b'
 client = Client(account_sid, auth_token)
 
 message = client.messages \
                 .create(
-                     body="Justin's Birthday is coming up next on March 3rd!",
+                     body="Christmas is coming up on December 25th!",
                      from_='+15102015515',
                      to='+15103298526'
                  )
